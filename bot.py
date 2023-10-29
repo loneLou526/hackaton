c0e4c76710c6d9d4c033008b81617a3413145810
loneLou526 <103879098+loneLou526@users.noreply.github.com>
2023-10-29 15:09:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,10 +10,10 @@ user_states = {}
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    remove_keyboard = telebot.types.ReplyKeyboardRemove()
     srch.way_search = message.chat.id
-    # srch.get_user(message.chat.id)
-    print(message.chat.id)
-    bot.send_message(message.chat.id, "Привет, я бот, который поможет тебе найти нужный документ.\n Чтобы начать введите команду /find")
+    srch.tag = 'инженер'
+    bot.send_message(message.chat.id, "Привет, я бот, который поможет тебе найти нужный документ.\n Чтобы начать введите команду /find",reply_markup=remove_keyboard)
 
 @bot.message_handler(commands=['find'])
 def find(message):
@@ -48,48 +48,50 @@ def handle_button_click(message):
         srch.way_search = "name"  
     else:
         srch.way_search = "type"  
+
     remove_keyboard = telebot.types.ReplyKeyboardRemove()
     bot.send_message(message.chat.id, f"Введите запрос на основе выбранной категории: ", reply_markup=remove_keyboard)
 
-def create_inline_keyboard(options):
-    options = ['Информационные технологии. ', 'Ведение электронной документации']
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    for option in options:
-        callback_data = f"option_{option}"
-        button = telebot.types.InlineKeyboardButton(text=option, callback_data=callback_data)
-        keyboard.add(button)
-    return keyboard
+# def create_inline_keyboard(options):
+#     options = ['Информационные технологии. ', 'Ведение электронной документации']
+#     keyboard = telebot.types.InlineKeyboardMarkup()
+#     for option in options:
+#         callback_data = f"option_{option}"
+#         button = telebot.types.InlineKeyboardButton(text=option, callback_data=callback_data)
+#         keyboard.add(button)
+#     return keyboard
 
 @bot.message_handler(func=lambda message: True)
 def handle_text(message):
     srch.search(message.text, srch.tag)
-    # if srch.answer:
-    #     keyboard = create_inline_keyboard(srch.answer)
-    #     bot.send_message(message.chat.id, "Вот документы найденные по вашему запросу.\nДля просмотра дополнительной информации нажмите на кнопку:", reply_markup=keyboard)
-    # else:
-    #     bot.send_message(message.chat.id, "По вашему запросу ничего не найдено")
-    bot.send_message(message.chat.id, f'{srch.answer}')
+    buttons = srch.answer.split('\n\n')
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True)
+    if srch.answer:
+        for but in buttons:
+            keyboard.add(but)
+        bot.send_message(message.chat.id, f'{srch.answer}', reply_markup=keyboard) #!!!!!! на данном этапе во время демонстрации работы бота, он ничего не
+                                                                                   #выведил, потому что у пользователя-инженера не было доступа к запрашиваемому
+                                                                                   #документу, и тг бот ругался, потому что не мог вывести пустую строку. 
+                                                                                   #Но я добавил проверку условием и теперь все в порядке ↓↓↓
+    else:
+        bot.send_message(message.chat.id, 'Подходящего вашему запросу документа не найдено, или у вас нет доступа.\nВведите /find чтоб еще раз произвести поиск')
+
+
+def send_file(name):
+    with open(fr"D:\Ярлыки\IT\Project\hackaton\files\{name}.pdf","rb") as f:
+        ret = f.read()
+    return ret
 
 """
-Данная функция к сожалению не успела доработаться 
+Данная функция к сожалению работает некорректно (не работает)
 """
-@bot.callback_query_handler(func=lambda call: True)
-def handle_button_click(call):
-    if call.data.startswith('option_'):
-        option = call.data[len('option_'):]
-        new_options = ["Новый вариант 1", "Новый вариант 2", "Новый вариант 3"]
-        new_keyboard = create_inline_keyboard(new_options)
-        
-        # Изменяем текст сообщения
-        bot.edit_message_text(f"Вы выбрали: {option}", call.message.chat.id, call.message.message_id)
-        
-        # Изменяем инлайн клавиатуру в сообщении
-        bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=new_keyboard)
+@bot.message_handler(func=lambda message: message.text in srch.answer.split('\n\n'))
+def handle_button_click(message):
+    remove_keyboard = telebot.types.ReplyKeyboardRemove()
+    print(send_file(message.text))
+    # bot.send_document(message.chat.id, send_file(message.text), reply_markup=remove_keyboard)
+    bot.send_message(message.chat.id, f"Введите запрос на основе выбранной категории: ", reply_markup=remove_keyboard)
+    
 
-"""
-Эта функция тоже в разработке
-"""
-@bot.message_handler(func=lambda message: True)
-def send_file(message):
-    pass
 bot.polling()
+
